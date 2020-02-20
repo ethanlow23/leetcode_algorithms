c35c8fd730027521723d98fa39efacea20de27ec
Ethan Low <ethanlow223@yahoo.com>
2020-02-20 11:00:21 -0800

    initial commit

---
+++ java/001_twoSum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<integer, integer> values = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int remainder = target - nums[i];
+            if (values.containsKey(remainder)) {
+                return new int[] { values.get(remainder), i };
+            }
+            values.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No Solution");
+    }
+}

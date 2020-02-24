b53f57ce988282e38cbc583ebb7c7ebbda756b0e
Ethan Low <ethanlow223@yahoo.com>
2020-02-24 14:36:01 -0800

    two sum

--- java/001_twoSum.java
+++ java/001_twoSum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<integer, integer> values = new HashMap<>();
+        Map<Integer, Integer> values = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int remainder = target - nums[i];
             if (values.containsKey(remainder)) {

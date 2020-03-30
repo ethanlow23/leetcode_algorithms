8b495909fa46083fdfd7e0e7738e4b73cfd3b391
Ethan Low <ethanlow223@yahoo.com>
2020-03-29 19:34:50 -0700

    intersection of two linked lists

---
+++ java/160_intersectionOfTwoLinkedLists.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pointerA = headA;
+        ListNode pointerB = headB;
+        while (pointerA != pointerB) {
+            pointerA = (pointerA == null) ? headB : pointerA.next;
+            pointerB = (pointerB == null) ? headA : pointerB.next;
+        }
+        return pointerA;
+    }
+}
